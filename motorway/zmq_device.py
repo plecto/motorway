339cadcaf23a420099ba1e3bab93539ef4aa09e8
Kristian Øllegaard <kristian@kristian.io>
2014-11-12 19:44:50 +0100

    Allow ZMQDevice to work with a larger amount of objects

--- motorway/zmq_device.py
+++ motorway/zmq_device.py
@@ -7,6 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 class ZMQDevice(object):
+    """
+    This is a fake "Queue" which allows multiple producers to push to it and multiple consumers to consume from it
+    """
     def __init__(self, queue, push_stream, pull_stream):
         setproctitle("data-pipeline: %s streamer" % queue)
         logger.debug("ZMQDevice %s %s %s" % (queue, push_stream, pull_stream))
@@ -26,10 +29,14 @@ class ZMQDevice(object):
             # Socket facing clients
             frontend = context.socket(zmq.PULL)
             frontend.bind(push_stream)  # Notice the name is inverted from socket
+            frontend.setsockopt(zmq.RCVHWM, 10000)  # Allow this "Queue" to contain up to 10k items
+            frontend.setsockopt(zmq.SNDHWM, 10000)  # Allow this "Queue" to contain up to 10k items
 
             # Socket facing services
             backend = context.socket(zmq.PUSH)
             backend.bind(pull_stream)  # Notice the name is inverted from socket
+            backend.setsockopt(zmq.RCVHWM, 10000)  # Allow this "Queue" to contain up to 10k items
+            backend.setsockopt(zmq.SNDHWM, 10000)  # Allow this "Queue" to contain up to 10k items
 
             zmq.device(zmq.STREAMER, frontend, backend)
         except Exception, e:
