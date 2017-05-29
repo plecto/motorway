62f13e3cd89e12b4a0b6768c96d77797ca233f96
Mikkel Clausen <mikkel.clausen@plecto.com>
2017-05-29 14:12:13 +0200

    motorway/contrib/amazone_kinesis: moved KinesisInsertIntersection to use put_records and handle ProvisionedThroughputExceededException

--- motorway/contrib/amazon_kinesis/intersections.py
+++ motorway/contrib/amazon_kinesis/intersections.py
@@ -1,6 +1,8 @@
 import json
 import boto.kinesis
+from motorway.decorators import batch_process
 from motorway.intersection import Intersection
+from time import sleep
 
 
 class KinesisInsertIntersection(Intersection):
@@ -15,15 +17,36 @@ class KinesisInsertIntersection(Intersection):
         return {
             'region_name': 'eu-west-1',
             # Add this or use ENV VARS
-            # 'aws_access_key_id': '',
-            # 'aws_secret_access_key': ''
+            #'aws_access_key_id': '',
+            #'aws_secret_access_key': ''
         }
 
-    def process(self, message):
-        self.conn.put_record(
-            self.stream_name,
-            json.dumps(message.content),
-            message.grouping_value
-        )
-        self.ack(message)
+    @batch_process(limit=500, wait=1)
+    def process(self, messages):
+        """
+        wait 1 second and get up to 500 items 
+        Each PutRecords request can support up to 500 records. 
+        Each record in the request can be as large as 1 MB, up to a limit of 5 MB for the entire request, including partition keys. 
+        Each shard can support writes up to 1,000 records per second, up to a maximum data write total of 1 MB per second.
+        This means we can run 2 intersections (2 x 500 records) submitting to the same shard before hitting the write limit (1000 records/sec)
+        If we hit the write limit we wait 2 seconds and try to send the records that failed again, rinse and repeat
+        :param messages: 
+        :return: 
+        """
+        records = []
+        for message in messages:
+            kinesis_record = {'PartitionKey': message.grouping_value, 'Data': json.dumps(message.content)}
+            records.append(kinesis_record)
+        response = self.conn.put_records(records, self.stream_name)
+        failed_records = []
+        for i, record in enumerate(response['Records']):
+            if len(record.get('ErrorCode', '')) > 0:
+                failed_records.append(messages[i])
+            else:
+                self.ack(messages[i])
+
+        if len(failed_records) > 0:
+            sleep(2)
+            self.process(failed_records)
+
         yield
