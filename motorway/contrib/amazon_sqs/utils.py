4a8adc791338c57f9bcecd6368967bd450cd2d2f
Mikkel Clausen <mikkel.clausen@plecto.com>
2018-01-10 11:07:19 +0100

    motorway/contrib/amazone_sqs: raise exception when sqs message is to big

--- motorway/contrib/amazon_sqs/utils.py
+++ motorway/contrib/amazon_sqs/utils.py
@@ -1,10 +1,17 @@
 import json
 from boto.sqs.message import Message
+from motorway.contrib.amazon_sqs.exceptions import SQSMessageToLarge
+
+SQS_MESSAGE_MAXIMUM_SIZE = 262144
 
 
 class SQSJSONMessage(Message):
     def encode(self, value):
-        return json.dumps(value)
+        s = json.dumps(value)
+        message_size = len(s.encode('utf-8'))
+        if message_size > SQS_MESSAGE_MAXIMUM_SIZE:
+            raise SQSMessageToLarge('message was %s in bytes and cant be bigger than %s' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
+        return s
 
     def decode(self, value):
         return json.loads(value)
