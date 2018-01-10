e202b891170bd1211a4c2a6c4ab5d2174c6a5ccf
Mikkel Clausen <mikkel.clausen@plecto.com>
2018-01-10 13:51:48 +0100

    motorway/contrib/amazon_sqs: cleanup for pull request

--- motorway/contrib/amazon_sqs/utils.py
+++ motorway/contrib/amazon_sqs/utils.py
@@ -2,7 +2,8 @@ import json
 from boto.sqs.message import Message
 from motorway.contrib.amazon_sqs.exceptions import SQSMessageToLarge
 
-SQS_MESSAGE_MAXIMUM_SIZE = 262144
+SQS_MESSAGE_MAXIMUM_SIZE = 262144  # The maximum is 262,144 bytes (256 KB).
+# https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/limits-messages.html
 
 
 class SQSJSONMessage(Message):
@@ -10,7 +11,7 @@ class SQSJSONMessage(Message):
         s = json.dumps(value)
         message_size = len(s.encode('utf-8'))
         if message_size > SQS_MESSAGE_MAXIMUM_SIZE:
-            raise SQSMessageToLarge('message was %s in bytes and cant be bigger than %s' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
+            raise SQSMessageToLarge('Message size of %sB exceeds the maximum SQS message size of %sB' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
         return s
 
     def decode(self, value):
