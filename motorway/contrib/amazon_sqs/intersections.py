531d886b55f0a5c0cc2be722be7351b8ed85e227
Mikkel Clausen <mikkel.clausen@plecto.com>
2018-06-11 13:32:41 +0200

    contrib/amazone_sqs: moved to boto3

--- motorway/contrib/amazon_sqs/intersections.py
+++ motorway/contrib/amazon_sqs/intersections.py
@@ -1,29 +1,27 @@
-import boto.sqs
-from motorway.contrib.amazon_sqs.utils import SQSJSONMessage
 from motorway.intersection import Intersection
+import boto3
+from utils import encode_to_json
 
 
 class SQSInsertIntersection(Intersection):
     queue_name = None
-    sqs_message_class = SQSJSONMessage
 
     def __init__(self, **kwargs):
         super(SQSInsertIntersection, self).__init__(**kwargs)
-        conn = boto.sqs.connect_to_region(**self.connection_parameters())
+        self.sqs = boto3.resource(**self.connection_parameters())
         assert self.queue_name, "Please define attribute queue_name on your SQSRamp"
-        self.queue = conn.create_queue(self.queue_name)
+        self.queue = self.sqs.create_queue(QueueName=self.queue_name)
 
     def connection_parameters(self):
         return {
             'region_name': 'eu-west-1',
+            'service_name': 'sqs'
             # Add this or use ENV VARS
             # 'aws_access_key_id': '',
             # 'aws_secret_access_key': ''
         }
 
     def process(self, message):
-        self.queue.write(
-            self.sqs_message_class(body=message.content)
-        )
+        self.queue.send_message(MessageBody=encode_to_json(message.content))
         self.ack(message)
         yield
--- motorway/contrib/amazon_sqs/utils.py
+++ motorway/contrib/amazon_sqs/utils.py
@@ -1,18 +1,13 @@
 import json
-from boto.sqs.message import Message
 from motorway.contrib.amazon_sqs.exceptions import SQSMessageToLarge
 
 SQS_MESSAGE_MAXIMUM_SIZE = 262144  # The maximum is 262,144 bytes (256 KB).
 # https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/limits-messages.html
 
 
-class SQSJSONMessage(Message):
-    def encode(self, value):
-        s = json.dumps(value)
-        message_size = len(s.encode('utf-8'))
-        if message_size > SQS_MESSAGE_MAXIMUM_SIZE:
-            raise SQSMessageToLarge('Message size of %sB exceeds the maximum SQS message size of %sB' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
-        return s
-
-    def decode(self, value):
-        return json.loads(value)
+def encode_to_json(value):
+    s = json.dumps(value)
+    message_size = len(s.encode('utf-8'))
+    if message_size > SQS_MESSAGE_MAXIMUM_SIZE:
+        raise SQSMessageToLarge('Message size of %sB exceeds the maximum SQS message size of %sB' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
+    return s
