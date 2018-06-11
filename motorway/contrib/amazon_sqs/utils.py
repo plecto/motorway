c56aefdd781a888b95342d7cc4e5cd73ef10f2d3
Mikkel Clausen <mikkel.clausen@plecto.com>
2018-06-11 18:00:14 +0200

    motorway/contrib/amazon_sqs: added encoder_class

--- motorway/contrib/amazon_sqs/utils.py
+++ motorway/contrib/amazon_sqs/utils.py
@@ -5,8 +5,8 @@ SQS_MESSAGE_MAXIMUM_SIZE = 262144  # The maximum is 262,144 bytes (256 KB).
 # https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/limits-messages.html
 
 
-def encode_to_json(value):
-    s = json.dumps(value)
+def encode_to_json(value, encoder_class=None):
+    s = json.dumps(value, cls=encoder_class)
     message_size = len(s.encode('utf-8'))
     if message_size > SQS_MESSAGE_MAXIMUM_SIZE:
         raise SQSMessageToLarge('Message size of %sB exceeds the maximum SQS message size of %sB' % (message_size, SQS_MESSAGE_MAXIMUM_SIZE))
