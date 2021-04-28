cb71333ef3a1590bf0c0fce3f7d5af0e9e13e39f
Sune Kjærgård <4482470+SuneKjaergaard@users.noreply.github.com>
2021-04-28 16:21:58 +0200

    Instead of calling create_queue on every instantiation of SQSInsertIntersection and SQSRamp, instead only create the queue if get_queue_by_name fails

--- motorway/contrib/amazon_sqs/ramps.py
+++ motorway/contrib/amazon_sqs/ramps.py
@@ -1,3 +1,5 @@
+from botocore.exceptions import ClientError
+
 from motorway.messages import Message
 from motorway.ramp import Ramp
 import boto3
@@ -12,7 +14,14 @@ class SQSRamp(Ramp):
         super(SQSRamp, self).__init__(*args, **kwargs)
         self.sqs = boto3.resource(**self.connection_parameters())
         assert self.queue_name, "Please define attribute queue_name on your SQSRamp"
-        self.queue = self.sqs.create_queue(QueueName=self.queue_name)
+        try:
+            self.queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
+        except ClientError as client_error:
+            # The queue doesn't exist and should be created
+            if client_error.response['Error']['Code'] == 'AWS.SimpleQueueService.NonExistentQueue':
+                self.queue = self.sqs.create_queue(QueueName=self.queue_name)
+            else:
+                raise client_error
         self.messages = {}
 
     def connection_parameters(self):
